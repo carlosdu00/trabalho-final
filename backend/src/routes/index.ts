0214b074d79f3a2ca938f4642cf1aa9e90d4050b
carlodu00 <carlosdu.carloseduardo@gmail.com>
2024-07-02 21:35:58 -0300

    fix routes.ts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import userRoutes from "./userRoutes";
 
+import userRoutes from "./userRoutes";
+import teamRoutes from "./userRoutes";
+import reviewRoutes from "./userRoutes";
 const appRouter = express();
 
 appRouter.use("/users", userRoutes);
+appRouter.use("/teams", userRoutes);
+appRouter.use("/reviews", userRoutes);
 
 export default appRouter;
