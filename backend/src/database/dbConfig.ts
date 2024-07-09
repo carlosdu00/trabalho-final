c89bf236e061044556a387191dc3e7923a1432c2
carlodu00 <carlosdu.carloseduardo@gmail.com>
2024-07-09 09:32:24 -0300

    ocultar credenciais com .env

--- backend/src/database/dbConfig.ts
+++ backend/src/database/dbConfig.ts
@@ -1,12 +1,14 @@
-// src/config/dbConfig.ts
 import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "hackathon_db",
-  password: "12345",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
 
 export default pool;
